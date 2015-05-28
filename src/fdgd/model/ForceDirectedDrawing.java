abae14901ca4159ab0cbc98a5e096877665ec369
Andre-S <andreschuelein@gmail.com>
2015-05-28 15:52:04 +0200

    added sliders in the GUI to control model parameters

--- src/fdgd/model/ForceDirectedDrawing.java
+++ src/fdgd/model/ForceDirectedDrawing.java
@@ -31,10 +31,68 @@ public class ForceDirectedDrawing extends NetworkBuilder {
 */	// 1 10 10000 1 works but slow
 	// 5 1 10000 1 works well, nodes a bit too spread
 	
+	
+	
 	double stabilizer1=1;
 	double stabilizer2=1;//numON;
 	
 	
+	/**
+	 * @return the c1
+	 */
+	public double getC1() {
+		return c1;
+	}
+
+	/**
+	 * @param c1 the c1 to set
+	 */
+	public void setC1(double c1) {
+		this.c1 = c1;
+	}
+
+	/**
+	 * @return the c2
+	 */
+	public double getC2() {
+		return c2;
+	}
+
+	/**
+	 * @param c2 the c2 to set
+	 */
+	public void setC2(double c2) {
+		this.c2 = c2;
+	}
+
+	/**
+	 * @return the c3
+	 */
+	public double getC3() {
+		return c3;
+	}
+
+	/**
+	 * @param c3 the c3 to set
+	 */
+	public void setC3(double c3) {
+		this.c3 = c3;
+	}
+
+	/**
+	 * @return the c4
+	 */
+	public double getC4() {
+		return c4;
+	}
+
+	/**
+	 * @param c4 the c4 to set
+	 */
+	public void setC4(double c4) {
+		this.c4 = c4;
+	}
+
 	public ForceDirectedDrawing(int numberOfNodes) {
 		super(numberOfNodes);
 		nodeLocations = new double [numON][2];
